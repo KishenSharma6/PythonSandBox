545b3893745cbd422f7a706758a818706357788f
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-18 09:35:53 -0700

    Added methods

--- src/human.py
+++ src/human.py
@@ -2,8 +2,15 @@ class Human:
     def __init__(self, name, age):
         self.name= name
         self.age= age
+        self.pocket= 0
     
     def introduce_self(self):
-        return("Hi there, my name is %s and I am %s years old." % (self.name, self.age))
+        print("Hi there, my name is %s and I am %s years old." % (self.name, self.age))
     
+    def found_change(self, amount:"float"):
+        self.pocket += amount
 
+    def on_hand(self):
+        """Returns how much money user has in wallet
+        """
+        return "$%s" % (self.pocket)
